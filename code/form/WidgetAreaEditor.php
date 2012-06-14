70a58c8e0227b53e3ead17a0cd932982d0453cd7
Tom Densham <tom.densham@studiobonito.co.uk>
2012-06-14 11:07:26 +0100

    BUGFIX Include missing css, image, and javascript resources

--- code/form/WidgetAreaEditor.php
+++ code/form/WidgetAreaEditor.php
@@ -23,6 +23,11 @@ class WidgetAreaEditor extends FormField {
 		Requirements::css('widgets/css/WidgetAreaEditor.css');
 		Requirements::javascript(THIRDPARTY_DIR . "/prototype/prototype.js");
 		Requirements::javascript(THIRDPARTY_DIR . '/behaviour/behaviour.js');
+        Requirements::javascript(THIRDPARTY_DIR . "/scriptaculous/builder.js");
+        Requirements::javascript(THIRDPARTY_DIR . "/scriptaculous/effects.js");
+        Requirements::javascript(THIRDPARTY_DIR . "/scriptaculous/dragdrop.js");
+        Requirements::javascript(THIRDPARTY_DIR . "/scriptaculous/control.js");
+        Requirements::javascript(THIRDPARTY_DIR . "/scriptaculous/slider.js");
 		Requirements::javascript('widgets/javascript/WidgetAreaEditor.js');
 
 		return $this->renderWith("WidgetAreaEditor");
