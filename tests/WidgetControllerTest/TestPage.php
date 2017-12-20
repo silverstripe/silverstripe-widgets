8583b894a1a5b0143f3197acc53b91b56a6a5762
Robbie Averill <robbie@silverstripe.com>
2017-12-21 11:16:48 +1300

    FIX Update tests to ensure TestPage owns its widget area

--- tests/WidgetControllerTest/TestPage.php
+++ tests/WidgetControllerTest/TestPage.php
@@ -4,18 +4,17 @@ namespace SilverStripe\Widgets\Tests\WidgetControllerTest;
 
 use Page;
 use SilverStripe\Dev\TestOnly;
-use SilverStripe\View\SSViewer;
 use SilverStripe\Widgets\Model\WidgetArea;
 
-/**
- * @package cms
- * @subpackage tests
- */
 class TestPage extends Page implements TestOnly
 {
     private static $table_name = 'TestPage';
 
-    private static $has_one = array(
-        'WidgetControllerTestSidebar' => WidgetArea::class
-    );
+    private static $has_one = [
+        'WidgetControllerTestSidebar' => WidgetArea::class,
+    ];
+
+    private static $owns = [
+        'WidgetControllerTestSidebar',
+    ];
 }
